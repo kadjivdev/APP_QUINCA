76f00d3621c7c76c9047a7f6a70d29f5ceccc76c
kadjivdev <kadjivdev@gmail.com>
2025-03-19 17:30:52 +0100

    debug du login form dans UserController 4

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::prefix("quinkadjiv_refont/public")->group(function () {
     Route::middleware('guest')->group(function () {
         Route::get('/', [UserController::class, 'showLogin'])->name('login-portail');
         // Route::post('/login', [UserController::class, 'login'])->name('login');
-        Route::post('/connexion', [UserController::class, 'login'])->name('connexion');
+        Route::any('/connexion', [UserController::class, 'login'])->name('connexion');
     });
 
     // Routes protégées
