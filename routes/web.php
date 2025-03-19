dfd929edc5a3b83b18c24ff73d047d1f4b63b63a
kadjivdev <kadjivdev@gmail.com>
2025-03-19 11:38:10 +0100

    web.php revued 6

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::prefix("quinkadjiv_refont/public")->group(function () {
     // Routes publiques
     Route::middleware('guest')->group(function () {
         Route::get('/', [UserController::class, 'showLogin'])->name('login-portail');
-        Route::post('/login', [UserController::class, 'login'])->name('login');
+        Route::any('/login', [UserController::class, 'login'])->name('login');
     });
 
     // Routes protégées
